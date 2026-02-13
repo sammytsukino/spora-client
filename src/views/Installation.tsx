8b47af9d337f5c491acbe212c9f98c8c7c513592
sammytsukino <samy.cabello.g@gmail.com>
2026-02-13 21:58:31 +0100

    feat: add API requests and implement authentication flow

--- src/views/Installation.tsx
+++ src/views/Installation.tsx
@@ -1,8 +1,13 @@
 
+import { useLocation } from "react-router-dom";
+
 export default function Installation() {
+  const location = useLocation();
+  const src = `/Installation.html${location.search || ""}`;
+
   return (
     <iframe
-      src="/Installation.html"
+      src={src}
       style={{ width: '100vw', height: '100vh', border: 'none', position: 'fixed', top: 0, left: 0, zIndex: 9999 }}
       title="Bouquet Generativo Installation"
       allowFullScreen
